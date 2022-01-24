1eabad1945b85dd16f94a7c4ea3d378f78412c28
Yuchira Athukorala <82855412+YuchiraJA@users.noreply.github.com>
2022-01-24 21:45:48 +0530

    Update NavBar.js
    code 4

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,25 +3,21 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
   render() {
     return (
-        <div>
-          <center>
- <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div>
+      <center>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
-  
-   <a style={{alignContent:'center'}} className="navbar-brand" href="#"> 	&nbsp; Food Recipe Web App	&nbsp; </a>
-  <div className="collapse navbar-collapse" id="navbarText">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">     
-        <a className="nav-link" href="#"> All Recipes <span className="sr-only">(current)</span></a>
-      
-      </li>
-    </ul>
-   
-  </div>
-</nav>
-</center>
-        </div>
-
+          <a style={{alignContent:'center'}} className="navbar-brand" href="/"><h3>&nbsp; Food Recipe Web App &nbsp;</h3></a>
+            <div className="collapse navbar-collapse" id="navbarText">
+              <ul className="navbar-nav mr-auto">
+                 <li className="nav-item active">     
+                    <a className="nav-link" href="/"> All Recipes <span className="sr-only">(current)</span></a>
+                </li>
+              </ul> 
+            </div>
+         </nav>
+      </center>
+   </div>
     )
   }
 }
